50079036694d743841799d2fe601c1ee1db1ba08
Machiaveli <kevin.s.ewald@gmail.com>
2015-11-09 11:38:50 +1000

    Resolved Conflicts

--- HLTHIR403C_CHCCS411C_AS3/Admin/customers.aspx.designer.cs
+++ HLTHIR403C_CHCCS411C_AS3/Admin/customers.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace HLTHIR403C_CHCCS411C_AS3.Admin {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
         
         /// <summary>
+        /// DataSourceUpperGridViewSelectedCustomer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource DataSourceUpperGridViewSelectedCustomer;
+        
+        /// <summary>
         /// ValidationSummary1 control.
         /// </summary>
         /// <remarks>
