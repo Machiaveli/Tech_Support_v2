c9098bcd54cfc802cc4deccd3eb99c10c1969dcb
eyehgc - Eunice Yeh <eunice422@gmail.com>
2015-11-09 20:38:56 +1000

    little bit of validation for customers.aspx.cs

--- HLTHIR403C_CHCCS411C_AS3/Admin/customers.aspx.cs
+++ HLTHIR403C_CHCCS411C_AS3/Admin/customers.aspx.cs
@@ -7,8 +7,10 @@
  * all products the user has registered. The user can then click the 'ARegister Product' button in order to register a product to a particular user.
  * 
  * Edit: Eunice Yeh - 6100439115
- * Added search bar and search function, as well as ability to edit after search
- * Date: 01/11/2015
+ * Date: 09/11/2015
+ * - Added search bar and search function
+ * - Enabled ability to edit after search
+ * - Added validation
  * 
  * Edit: Kevin Ewald - 8103677414
  * Date: 9/11/2015
@@ -52,32 +54,62 @@ namespace HLTHIR403C_CHCCS411C_AS3.Admin
             // binds the correct data source to the correct GridView depending on the dropdownlist Selection
             if (dropDownSearchFilter.SelectedValue.ToString().Trim().Equals("customerID"))
             {
-                GridViewDisplayCustomers.DataSourceID = "DataSourceSearchCustByID";
-                GridViewDisplayCustomers.DataBind();
-                GridViewCustomers.Visible = false;
-                GridViewDisplayCustomers.Visible = true;
-                lblCustomerRegistrationResults.Visible = true;
-                GridViewDisplayCustomers.SelectedIndex = -1;
-                GridViewCustomers.SelectedIndex = -1;
+                try
+                {
+                    GridViewDisplayCustomers.DataSourceID = "DataSourceSearchCustByID";
+                    GridViewDisplayCustomers.DataBind();
+                    GridViewCustomers.Visible = false;
+                    GridViewDisplayCustomers.Visible = true;
+                    lblCustomerRegistrationResults.Visible = true;
+                    GridViewDisplayCustomers.SelectedIndex = -1;
+                    GridViewCustomers.SelectedIndex = -1;
+                }
+                catch (Exception ex)
+                {
+                    lblCustomerRegistrationResults.Text = ex.Message + " Please ensure that you have entered a valid customer ID.";
+                    lblCustomerRegistrationResults.Visible = true;
+                    GridViewCustomers.Visible = false;
+                    GridViewDisplayCustomers.Visible = false;
+                    btnRegister.Visible = false;
+                }
             }
 
             else if (dropDownSearchFilter.SelectedValue.ToString().Trim().Equals("customerLastName"))
             {
-                GridViewDisplayCustomers.DataSourceID = "DataSourceSearchCustByLastName";
-                GridViewDisplayCustomers.DataBind();
-                GridViewCustomers.Visible = false;
-                GridViewDisplayCustomers.Visible = true;
-                lblCustomerRegistrationResults.Visible = true;
-                GridViewDisplayCustomers.SelectedIndex = -1;
-                GridViewCustomers.SelectedIndex = -1;
+                try
+                {
+                    GridViewDisplayCustomers.DataSourceID = "DataSourceSearchCustByLastName";
+                    GridViewDisplayCustomers.DataBind();
+                    GridViewCustomers.Visible = false;
+                    GridViewDisplayCustomers.Visible = true;
+                    lblCustomerRegistrationResults.Visible = true;
+                    GridViewDisplayCustomers.SelectedIndex = -1;
+                    GridViewCustomers.SelectedIndex = -1;
+                }
+                catch (Exception ex)
+                {
+                    lblCustomerRegistrationResults.Text = ex.Message;
+                    lblCustomerRegistrationResults.Visible = true;
+                }
             }
             else if (dropDownSearchFilter.SelectedValue.ToString().Trim().Equals("ListAllCustomers"))
             {
-                GridViewCustomers.Visible = true;
-                GridViewDisplayCustomers.Visible = false;
-                lblCustomerRegistrationResults.Visible = false;
-                GridViewDisplayCustomers.SelectedIndex = -1;
-                GridViewCustomers.SelectedIndex = -1;
+                try
+                {
+                    GridViewCustomers.Visible = true;
+                    GridViewDisplayCustomers.Visible = false;
+                    lblCustomerRegistrationResults.Visible = false;
+                    GridViewDisplayCustomers.SelectedIndex = -1;
+                    GridViewCustomers.SelectedIndex = -1;
+                }
+                catch (Exception ex)
+                {
+                    lblCustomerRegistrationResults.Text = ex.Message;
+                    lblCustomerRegistrationResults.Visible = true;
+                    GridViewCustomers.Visible = false;
+                    GridViewDisplayCustomers.Visible = false;
+                    btnRegister.Visible = false;
+                }
             }
         }
 
@@ -93,7 +125,7 @@ namespace HLTHIR403C_CHCCS411C_AS3.Admin
             else
             {
                 txtSearchQuery.Enabled = true;
-                lblSearchRecords.ForeColor = System.Drawing.Color.Yellow;
+                lblSearchRecords.ForeColor = System.Drawing.Color.White;
                 btnSearch.Text = "Search";
                 txtSearchQuery.Enabled = true;
             }
